85bb7c0059394a1270d2945908cf1d67298fc594
RafifaSahirah <rafifasahirah6@gmail.com>
2025-05-14 14:19:30 +0700

    update config: change name of model

--- dashboard/config.py
+++ dashboard/config.py
@@ -10,7 +10,7 @@ BASE_URL = "https://openrouter.ai/api/v1"
 
 # FAISS index paths
 INDEX_PATH = "../models/faiss_index.bin"
-METADATA_PATH = "../models/embeddings_faiss.pkl"
+METADATA_PATH = "../models/embeddings.pkl"
 
 # Model for embeddings
 MODEL_NAME = "all-MiniLM-L6-v2"
